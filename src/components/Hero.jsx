0e23ff3c99ad07c6fd99614f6cf9949cdc43dfff
blackingg <mudet04@gmail.com>
2024-09-15 11:04:04 +0100

    #

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="relative bg-heroBackground bg-no-repeat bg-center bg-cover h-screen flex flex-col justify-center items-center">
       <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-sm"></div>
-      <h1 className="text-5xl font-bold mb-4 text-center text-white z-10">
+      <h1 className="font-sans text-5xl font-bold mb-4 text-center text-white z-10">
         Medical Information for the Modern Era
       </h1>
       <p className="text-xl mb-8 text-center text-[#b1b1b1] max-w-2xl z-10">

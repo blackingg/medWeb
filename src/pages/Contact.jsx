06f64124dd8f19af5894691fce8912ceabf62884
blackingg <mudet04@gmail.com>
2024-09-21 20:15:51 +0100

    #

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { IoArrowBackCircle } from "react-icons/io5";
 
 const Contact = () => {
   const navigate = useNavigate();
